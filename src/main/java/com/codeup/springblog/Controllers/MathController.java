bd1106e71d468865a574bad48c403ff1f3e219f5
KennethMcInturf <kennethcmcinturf@gmail.com>
2019-01-07 14:31:52 -0600

    finished exercise

---
+++ src/main/java/com/codeup/springblog/Controllers/MathController.java
@@ -0,0 +1,26 @@
+package com.codeup.springblog.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @Controller
+    class doThatMath{
+        @GetMapping("/{operations}/{numberOne}/{unimportant}/{numberTwo}")
+        @ResponseBody
+        public int doMath(@PathVariable String operations, @PathVariable int numberOne, @PathVariable int numberTwo){
+            if (operations.equals("add")){
+                return numberOne + numberTwo;
+            }else if (operations.equals("subtract")){
+                return numberOne + numberTwo;
+            }else if (operations.equals("multiply")){
+                return numberOne + numberTwo;
+            }else{
+                return numberOne / numberTwo;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/codeup/springblog/Controllers/PostController.java
@@ -0,0 +1,34 @@
+package com.codeup.springblog.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+    @Controller
+    class controlPost{
+        @RequestMapping(path = "/posts", method = RequestMethod.GET)
+        @ResponseBody
+        public String post() {
+            return "posts index page";
+        }
+
+        @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+        @ResponseBody
+        public String indivPost() {
+            return "view an individual post";
+        }
+
+        @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+        @ResponseBody
+        public String postForm() {
+            return "view the form for creating a post";
+        }
+
+        @RequestMapping(path = "/posts/create")
+        @PostMapping
+        public String createPost() {
+            return "create a new post";
+        }
+    }
+}
