ee5bf4b570aecaab26ca871827b76078f5a631ec
KennethMcInturf <kennethcmcinturf@gmail.com>
2019-01-08 15:17:59 -0600

    can create new posts

--- src/main/java/com/codeup/springblog/Controllers/PostController.java
+++ src/main/java/com/codeup/springblog/Controllers/PostController.java
@@ -20,18 +20,6 @@ public class PostController {
     class controlPost{
         @RequestMapping(path = "/posts", method = RequestMethod.GET)
         public String post(Model model) {
-            Post post1 = new Post();
-            Post post2 = new Post();
-            Post post3 = new Post();
-            post1.setTitle("Post One");
-            post2.setTitle("Post Two");
-            post3.setTitle("Post Three");
-            post1.setBody("Here's some content");
-            post2.setBody("Here's some more content");
-            post3.setBody("Here's even more content");
-            postService.create(post1);
-            postService.create(post2);
-            postService.create(post3);
             List<Post> posts = postService.all();
             model.addAttribute("posts", posts);
             return "posts/index";
@@ -45,15 +33,19 @@ public class PostController {
         }
 
         @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-        @ResponseBody
-        public String postForm() {
-            return "view the form for creating a post";
+        public String postForm(Model model) {
+            model.addAttribute("post", new Post());
+            return "posts/create";
         }
 
         @RequestMapping(path = "/posts/create")
         @PostMapping
-        public String createPost() {
-            return "create a new post";
+        public String createPost(@ModelAttribute Post post) {
+            Post newPost = new Post();
+            newPost.setTitle(post.getTitle());
+            newPost.setBody(post.getBody());
+            postService.create(newPost);
+            return "posts/index";
         }
     }
 
