194b0f71f5a60ddb01036be51da414987f41d756
KennethMcInturf <kennethcmcinturf@gmail.com>
2019-01-08 12:00:32 -0600

    finished up to service

--- src/main/java/com/codeup/springblog/Controllers/PostController.java
+++ src/main/java/com/codeup/springblog/Controllers/PostController.java
@@ -1,22 +1,29 @@
 package com.codeup.springblog.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class PostController {
     @Controller
     class controlPost{
         @RequestMapping(path = "/posts", method = RequestMethod.GET)
-        @ResponseBody
-        public String post() {
-            return "posts index page";
+        public String post(Model model) {
+            Post post1 = new Post("Post One", "This is my new posts");
+            Post post2 = new Post("Post Two", "Here's some other stuff");
+            Post[] posts = {post1,post2};
+            model.addAttribute("posts", posts);
+            return "index";
         }
 
-        @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-        @ResponseBody
-        public String indivPost(@PathVariable long id) {
-            return "view an individual post " + id;
+        @RequestMapping(path = "/posts/{title}", method = RequestMethod.GET)
+        public String indivPost(@PathVariable String title, Model model) {
+            Post post1 = new Post("Post One", "This is my new posts");
+            model.addAttribute("post", post1);
+            return "show";
         }
 
         @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
---
+++ src/main/java/com/codeup/springblog/Controllers/PostService.java
@@ -0,0 +1,4 @@
+package com.codeup.springblog.Controllers;
+
+public class PostService {
+}
